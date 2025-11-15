2b1d648dda78b9cdd4481655fd096803c8c03641
kevinletexier <kevin.le.texier@gmail.com>
2025-11-15 22:55:28 +0100

    add method add_a_book to Cart

--- src/cart.py
+++ src/cart.py
@@ -1,4 +1,11 @@
 class Cart:
 
+    _books = 0
+
     def get_bill(self):
+        if self._books > 0:
+            return 8
         return 0
+
+    def add_a_book(self):
+        self._books += 1
--- tests/test_buying_books.py
+++ tests/test_buying_books.py
@@ -6,3 +6,8 @@ class TestBuyingBooks:
     def test_should_return_0_if_no_books_to_buy(self):
         cart = Cart()
         assert cart.get_bill() == 0
+
+    def test_should_return_8_if_cart_has_1_book(self):
+        cart = Cart()
+        cart.add_a_book()
+        assert cart.get_bill() == 8
